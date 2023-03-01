7279bfd553e75bb7d1369af68c9cd56494d8672b
Stefanie Brandl <stefanie.brandl@sap.com>
2023-03-01 13:20:07 +0100

    New activity added (empty)

---
+++ app/src/main/java/com/example/cork360/Data.java
@@ -0,0 +1,14 @@
+package com.example.cork360;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Data extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+    }
+}
